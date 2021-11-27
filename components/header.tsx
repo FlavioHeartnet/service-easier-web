9a92ca563f709a7327f624824bdaab533c820cb1
Flavio <flavionogueirabarros@gmail.com>
2021-11-27 17:56:50 -0300

    feat/home and header

---
+++ components/header.tsx
@@ -0,0 +1,26 @@
+import { Menu, Dropdown} from 'semantic-ui-react'
+import styles from './../styles/Header.module.scss'
+import {useRouter} from 'next/router'
+import { signOut  } from "firebase/auth";
+import {auth} from './../firebase'
+export default function Header(props){
+    const router = useRouter()
+    function handleroute(url){
+        router.push(url)
+    }
+
+    function logout(){
+        signOut(auth).then(()=>{
+
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+    return (<div>
+        <Menu fixed='top' secondary>
+        <Menu.Item><b>Service Easier</b></Menu.Item>
+        <Menu.Item onClick={()=> handleroute('/home')}><b>Home</b></Menu.Item>
+        <Menu.Item onClick={logout} position='right'>Sair</Menu.Item>
+        </Menu>
+    </div>)
+}
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,5 +1,29 @@
+import { onAuthStateChanged  } from "firebase/auth";
+import {auth} from './../firebase'
+import styles from './../styles/Home.module.scss'
+import {useRouter} from 'next/router'
+import { useEffect } from "react";
+import Header from './../components/header'
+import {Container} from 'semantic-ui-react'
 export default function Home(){
+    const router = useRouter()
+    useEffect(()=>{
+        onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                
+            } else {
+                router.push("/")
+            }
+          });
+    })
     return (
-        <div><h1>Home</h1></div>
+        <div>
+            <Header />
+            <Container>
+                <br/>
+                <h1>Home</h1>
+            </Container>
+            
+        </div>
     )
 }
