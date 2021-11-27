259e62a343b862e2ce645ac2f5c3dbe7cc519ff6
Flavio <flavionogueirabarros@gmail.com>
2021-11-27 16:41:20 -0300

    feat/firebase auth

---
+++ firebase.ts
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBRZWp75q860y1KJ32HaqTgJ4eeyl11v0A",
+  authDomain: "service-easier.firebaseapp.com",
+  projectId: "service-easier",
+  storageBucket: "service-easier.appspot.com",
+  messagingSenderId: "786858898622",
+  appId: "1:786858898622:web:4e67f46c86c06835981aef",
+  measurementId: "G-TGMQ6HE1EQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const GoogleProvider = new GoogleAuthProvider();
+export const auth = getAuth(app)
+export const analytics = getAnalytics(app);
+export const db = getFirestore(app)
+
+export default app
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,83 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
-import {Form, Button, Input} from 'semantic-ui-react'
+import styles from '../styles/Login.module.scss'
+import {Form, Button, Message, Segment} from 'semantic-ui-react'
 import { useState } from 'react'
+import {auth, GoogleProvider} from './../firebase'
+import { signInWithEmailAndPassword,signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {useRouter} from 'next/router'
+import localizeErrorMap from './../Utils/firebaseMessagesBr'
 
-export default function Home() {
+export default function Login() {
+  const router = useRouter()
+  auth.useDeviceLanguage()
+
   const [isLoading, setLoading] = useState(false)
+  const [email, setEmail] =  useState("")
+  const [password, setPassword] = useState("")
+  const [isFormSucess, setFormSucess] = useState({})
+  const [formMessage, setFormMessage] = useState({
+      success: true,
+      error:false,
+      header:'',
+      content:''
+  })
+  function loginGoogle(){
+    signInWithPopup(auth, GoogleProvider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      router.push('/home')
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorMessage = localizeErrorMap(error);
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      setFormSucess({
+        error: true
+    })
+    setFormMessage({
+        error: true,
+        success:false,
+        header: 'Não foi possivel logar :(',
+        content: errorMessage
+      })
+      setLoading(false)
+    });
+  }
   function login(){
     setLoading(true)
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    setLoading(false)
+    const user = userCredential.user;
+    router.push('/home')
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = localizeErrorMap(error)
+    
+
+    setFormSucess({
+      error: true
+  })
+  setFormMessage({
+      error: true,
+      success:false,
+      header: 'Não foi possivel logar :(',
+      content: errorMessage
+    })
+    setLoading(false)
+  });
   }
 
   return (
     <div className={styles.container}>
+      <div className={styles.backgroung}>
       <Head>
         <title>Organização de serviços prestados</title>
         <meta name="description" content="Aqui você pode organizar seus serviços" />
@@ -19,6 +85,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <Segment raised>
         <h1 className={styles.title}>
           Service Easier <br/><a href="">Gestão de Serviços!</a>
         </h1>
@@ -26,41 +93,41 @@ export default function Home() {
         <p className={styles.description}>
           Faça seu login aqui!
         </p>  
-      <Form onSubmit={login}> 
-      <Form.Field
-        id='form-input-control-last-name'
-        control={Input}
-        label='E-mail'
-        placeholder='Digite seu E-mail'
-        type='email'
-        required
-      />
-      <Form.Field
-        id='form-input-control-last-name'
-        control={Input}
-        label='Senha'
-        placeholder='Digite sua Senha'
-        type='password'
-        required
-      />
-      <Button loading={isLoading} primary>Entrar</Button>
-      <Button type={'button'}>Não tem acesso? cadastre-se</Button>
+      <Form onSubmit={login} {...isFormSucess}>
+      <Message
+                    {...formMessage}          
+                /> 
+      <Form.Field>
+          <label>E-mail</label>
+          <input placeholder={'Digite seu email'} required onChange={(e)=> setEmail(e.target.value)} type='email' />
+      </Form.Field>
+      <Form.Field>
+          <label>Senha</label>
+          <input placeholder={'Digite sua senha'} required onChange={(e)=> setPassword(e.target.value)} type='password' />
+      </Form.Field>
+      <br/>
+      <div className={styles.buttons}>
+        <Button loading={isLoading} primary>Entrar</Button>
+        <Button type={'button'} color='red' onClick={loginGoogle}>Entrar com Google</Button>
+        <Button type={'button'}>Não tem acesso? cadastre-se</Button>
+      </div>
       </Form>
-
+      </Segment>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://portifolio-284f2.web.app/#home"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by {' '}
+          
+            Flavio Nogueira
+          
         </a>
       </footer>
+      </div>
     </div>
   )
 }
