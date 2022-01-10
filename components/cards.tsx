c8c174453e5979ab580d26df7c36c0deee57121c
Flavio Nogueira <flavionogueirabarros@gmail.com>
2022-01-10 14:58:23 -0300

    feat: :sparkles: Startind creating home page

--- components/cards.tsx
+++ components/cards.tsx
@@ -1,38 +1,15 @@
-import { Card } from "semantic-ui-react";
+import { Card, Grid, Icon, IconGroup } from "semantic-ui-react";
 import  './../styles/Home.module.scss'
 export default function Cards(){
-
-    const items = [
-        {
-          header: 'Numero de Serviços feitos no mês',
-          description:
-            '25',
-          meta: 'Números de serviços prestados a clientes.',
-          color: 'pink',
-          fluid: true
-        },
-        {
-          header: 'Rentabilidade do mês',
-          description:
-            'R$ 500,00',
-          meta: 'Valores atualizados sem o desconto de comissão.',
-          color: 'pink',
-          fluid: true
-        },
-        {
-          header: 'Lucro previsto do mês',
-          description:
-            'R$ 250,00',
-          meta: 'Valores previstos a serem recebidos',
-          color: 'pink',
-          fluid: true
-        },
-      ]
-
-
     return(
         <div>
-            <Card.Group doubling itemsPerRow='3' centered items={items} />
+            <Grid>
+                <Grid.Row>
+                    <Icon color='pink' circular inverted size='big' name='add'/>
+                    <Icon color='pink' circular inverted size='big' name='search'/>
+                    <Icon color='pink' circular inverted size='big' name='user'/>
+                </Grid.Row>
+            </Grid>
         </div>
     )
 }
--- pages/home.tsx
+++ pages/home.tsx
@@ -8,25 +8,14 @@ export default function Home(){
     updateTitlePage("Olá, " + name + " ;)")
     return (
         <>
-        <HeaderMenu >
-
-            <Dimmer.Dimmable as={Segment} dimmed>
-                <Dimmer active>
-                <Header as='h2' icon inverted>
-                <Icon name='heart' />
-                    Esta pagina ainda esta em construção :) <br/> em breve ela estará disponivel
-                </Header>
-                </Dimmer>
-                <Container>
-                <br/><br/>
-                <Cards/>
-                <br/><br/>
-                <HomeChart/>
-                </Container>
-                </Dimmer.Dimmable>
-            
+        <HeaderMenu >    
+            <Container>
+            <br/><br/>
+            <Cards/>
+            <br/><br/>
+            <HomeChart/>
+            </Container>
             <br/>
-            
         </HeaderMenu>
         
         </>
